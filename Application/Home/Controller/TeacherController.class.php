0349330e2412f13ed9a5dad501b62dd8a00002e4
xuzhen <2585458279@qq.com>
2016-12-05 19:54:20 +0800

    xiugai

--- Application/Home/Controller/TeacherController.class.php
+++ Application/Home/Controller/TeacherController.class.php
@@ -62,6 +62,7 @@ class TeacherController extends Controller
                   }
                   $getdate = array(); //定义新数组
                   $result = $this->toJson($code, $message, $data);
+                  echo   $result;
       }
 
     public function getSchedule()
@@ -253,4 +254,5 @@ class TeacherController extends Controller
       return $datas;
 
     }
+
 }
